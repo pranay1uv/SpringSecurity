90c054f888eed5bfed4b6b6cf7e6d5e844e2a741
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-10-22 18:48:30 +0530

    Creating user with roles Storing the user details

--- se-security-01/src/main/java/com/security/app/controller/SecurityAppConfig.java
+++ se-security-01/src/main/java/com/security/app/controller/SecurityAppConfig.java
@@ -1,7 +1,17 @@
 package com.security.app.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 // This is class is going to create a security chain filter
 // Sceurity chain filter has a list of filters
@@ -15,5 +25,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityAppConfig {
-
+	
+	
+	@Bean
+	public InMemoryUserDetailsManager setUpUser() {
+		
+		ArrayList<GrantedAuthority> authoritiesList = new ArrayList<>();
+		authoritiesList.add(new SimpleGrantedAuthority("admin"));
+		authoritiesList.add(new SimpleGrantedAuthority("Visitor"));
+		
+		UserDetails newuser = new User("pranay", "password", authoritiesList);
+		
+		InMemoryUserDetailsManager inMemoryUserDetails = new InMemoryUserDetailsManager();
+		inMemoryUserDetails.createUser(newuser);
+		
+		return inMemoryUserDetails;
+		
+	}
+	
+	@Bean
+	PasswordEncoder passwordencoder() {
+		
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	
 }
