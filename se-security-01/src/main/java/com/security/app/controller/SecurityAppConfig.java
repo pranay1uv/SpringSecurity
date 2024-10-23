a49d6359fb52b1eeb67d85dd55fd14831d46e6d4
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-10-23 19:12:03 +0530

    Setting Up Users using UserBuilder Class

--- se-security-01/src/main/java/com/security/app/controller/SecurityAppConfig.java
+++ se-security-01/src/main/java/com/security/app/controller/SecurityAppConfig.java
@@ -9,45 +9,61 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-// This is class is going to create a security chain filter
-// Sceurity chain filter has a list of filters
-// the primary purpose of this security filter chain
-// is to validate a client request
-// i.e before hitting the one our end points
-// these filters will com into the picture based in the type of request
-// and act accordingly
-// Note: if any of the filters doesnt have ny work or role
-// those filters will be skipped
+     
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityAppConfig {
 	
 	
 	@Bean
-	public InMemoryUserDetailsManager setUpUser() {
-		
-		ArrayList<GrantedAuthority> authoritiesList = new ArrayList<>();
-		authoritiesList.add(new SimpleGrantedAuthority("admin"));
-		authoritiesList.add(new SimpleGrantedAuthority("Visitor"));
-		
-		UserDetails newuser = new User("pranay", "password", authoritiesList);
-		
-		InMemoryUserDetailsManager inMemoryUserDetails = new InMemoryUserDetailsManager();
-		inMemoryUserDetails.createUser(newuser);
-		
-		return inMemoryUserDetails;
-		
+	public InMemoryUserDetailsManager setUpUsers() {
+		 UserDetails pranayuser = User 
+				.withUsername("pranay")
+				.password("$2a$10$wTp5pQPhxgo9F9V0XNHQgePqHO9SitpaEAndf5SLrVwaFHy7Xj5vO")
+				.roles("Admin","visitor")
+				.build();
+		 
+		 UserDetails georgeuser = User
+				 .withUsername("george")
+				 .password("$2a$10$MVoxdVxjPYNL/RokzcmsuevUajzgq/etgow0BJwNuSlJbCzkgHg.u")
+				 .roles("customer","visitor")
+				 .build();
+		 return new InMemoryUserDetailsManager(pranayuser,georgeuser);	
 	}
 	
 	@Bean
 	PasswordEncoder passwordencoder() {
-		
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
+	
+	
+//	@Bean
+//	public InMemoryUserDetailsManager setUpUser() {
+//		
+		
+//		ArrayList<GrantedAuthority> authoritiesList = new ArrayList<>();
+		// In the below lines we are appending one element to the authoritiesList
+//		authoritiesList.add(new SimpleGrantedAuthority("admin"));
+//		authoritiesList.add(new SimpleGrantedAuthority("Visitor"));
+//		
+//		UserDetails newuser = new User("pranay", "password", authoritiesList);
+//		
+//		InMemoryUserDetailsManager inMemoryUserDetails = new InMemoryUserDetailsManager();
+//		inMemoryUserDetails.createUser(newuser);
+//		
+//		return inMemoryUserDetails;
+//		
+//	}
+//	
+//	@Bean
+//	PasswordEncoder passwordencoder() {
+//		
+//		return NoOpPasswordEncoder.getInstance();
+//	}
 	
 	
 }
